4168aeb975e475a28849f4858acaf5399c60d6fa
anasraza57 <2f4BC6nwdP87iy>
2020-06-14 02:00:31 +0500

    Splash added

---
+++ app/src/main/java/com/example/kaamwala/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.kaamwala;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setContentView(R.layout.activity_splash);
+
+        ImageView iv = findViewById(R.id.logo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        iv.startAnimation(myanim);
+        final Intent intent = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
